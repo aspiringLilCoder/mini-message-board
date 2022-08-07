0a0620f85f3a561ff3330b9e11e1f59b171f177b
aspiringLilCoder <aspiringLilCoder@gmail.com>
2022-08-07 12:21:58 +0800

    done

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", (req, res, next) => {
+  const newMessage = {
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date(),
+  };
+  messages.push(newMessage);
+  res.redirect("/");
+});
+
+module.exports = router;
